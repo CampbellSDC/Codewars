3faa92757618d2e1c91385e0476bf5bc564dc208
Stephen Campbell <scampbellatc@gmail.com>
2022-04-05 13:26:46 -0400

    New Codewars challege answer

---
+++ codewar1.js
@@ -0,0 +1,10 @@
+//Documenting each completed Codewars kyu
+
+//Create  function that removes the first and last letters from a string
+
+function removeChar(str){
+    return str.slice(1,str.length-1 )
+   
+   };
+
+
---
+++ codewar2.js
@@ -0,0 +1,8 @@
+//Create a function that returns the smallest integer from an array
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+      
+    }
+  }
